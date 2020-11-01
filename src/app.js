43681cf15f4595cd264c568423e36e5b7bd2470e
paulodevsilva <silva.paulohr@gmail.com>
2020-11-01 15:45:29 -0300

    disable morgan and swagger

--- src/app.js
+++ src/app.js
@@ -2,17 +2,17 @@ require('dotenv').config();
 
 import express from 'express';
 import bodyParser from 'body-parser';
-import morgan from 'morgan';
+// import morgan from 'morgan';
 import cors from 'cors';
 import multer from 'multer';
 import path from 'path';
-import swagger from 'swagger-ui-express';
+// import swagger from 'swagger-ui-express';
 
 import routes from './routes';
 import upload_config from './config/upload';
 import './config/database';
 import model from './models';
-import swagger_file from './swagger_output.json';
+// import swagger_file from './swagger_output.json';
 
 const app = express();
 const upload = multer(upload_config);
@@ -26,9 +26,9 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use('/files', express.static(path.resolve(__dirname, '..','tmp','uploads')));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(cors());
-app.use('/doc', swagger.serve, swagger.setup(swagger_file))
+// app.use('/doc', swagger.serve, swagger.setup(swagger_file))
  routes(app)
 // require('./routes')(app)
 
